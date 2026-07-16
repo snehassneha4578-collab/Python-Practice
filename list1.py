9b6f9744e85f6cf27410b039f7788388b0800263
Sneha S <snehassneha4578@gmail.com>
2026-07-16 11:33:13 +0530

    Add files via upload
    List

---
+++ list1.py
@@ -0,0 +1,11 @@
+numbers=[10,0,-1,7,4,9,16,12]
+print(numbers)
+numbers[1:4]=[45,46,47]
+print(numbers)
+numbers.append(7)
+print(numbers)
+numbers.extend([1,2,3,4,5,6,7,8,9])
+print(numbers)
+print(numbers.pop())
+
+

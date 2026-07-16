95eee86e4d7c5dee3ddff29fa052cd8d716f027e
Sneha S <snehassneha4578@gmail.com>
2026-07-16 11:32:42 +0530

    Add files via upload
    Random Values

---
+++ random.py
@@ -0,0 +1,12 @@
+import random
+
+a=random.randint(1,3)
+print(a)
+a=random.randint(1,3)
+print(a)
+a=random.uniform(1,3)
+l=[2,5,90,-5,8,9,12,56]
+a=random.choice(l)
+print(a)
+random.shuffle(l)
+print(l)
